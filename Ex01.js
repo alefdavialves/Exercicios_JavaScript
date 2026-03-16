82ae4da1477f6dfa70464d229c911a9fcc877027
Álef Davi <alefdavialves@gmail.com>
2026-03-16 15:37:14 -0300

    Adicionando Arquivos

---
+++ Ex01.js
@@ -0,0 +1,30 @@
+// // 1 - Crie um jogo onde o computador gera um número aleatório de 1 a 20, e o usuário
+// // precisa adivinhar qual é. O jogo deve dar dicas se o número digitado for maior ou menor
+// // que o número secreto. O jogo só termina quando o usuário acertar.
+// // Dica: utilize a biblioteca Math para gerar os números aleatórios;
+
+
+// var numeroAleatorio = Math.floor(Math.random() * 20) + 1;
+// var numeroDigitado;
+
+// window.alert("Bem vindo ao jogo! Acerte o numero de 0 a 20: ");
+// numeroDigitado = Number(prompt (" \n Digite seu chute"));
+
+// while (numeroAleatorio != numeroDigitado) {
+
+//     if (numeroAleatorio < numeroDigitado) {
+
+//         alert("O numero é menor");
+//     }
+
+//     else {
+
+//         alert("O numero é maior");
+
+//     }
+
+//     numeroDigitado = Number(prompt (" \n Digite outro numero"));
+
+// }
+
+// alert("Parabéns! Você acertou 🎉");
---
+++ Ex02.js
@@ -0,0 +1,33 @@
+// let opcoes = ["pedra", "papel", "tesoura"];
+
+
+// let jogadaAleatoria = Math.floor(Math.random() * 3);
+// let jogadaComputador = opcoes[jogadaAleatoria];
+
+// window.alert("Bem vindo ao jogo Pedra, Papel, Tesoura!"); 
+// let jogadaUsuario = prompt (" \n Digite sua jogada").toLowerCase();
+
+// if (jogadaUsuario === jogadaComputador) {
+
+//     alert ("Partida empatada");
+
+// }
+
+// else if (
+
+// (jogadaUsuario === "pedra" && jogadaComputador === "tesoura") ||
+// (jogadaUsuario === "tesoura" && jogadaComputador === "papel") ||
+// (jogadaUsuario === "papel" && jogadaComputador === "pedra") 
+
+// ) {
+
+
+// alert ("Parabéns! Você venceu");
+
+// }
+
+// else {
+
+// alert(`Você perdeu! O computador escolheu ${jogadaComputador} e você ${jogadaUsuario}`);
+
+// }
